711360b2c2b99d56d760d6c2f1cff9d0913fe6d8
Anton <wrstrass@gmail.com>
2023-04-23 04:06:08 +0300

    Change model RefreshToken

--- models/refresh_token.py
+++ models/refresh_token.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from models import BaseModel
 
@@ -6,7 +6,7 @@ from models import BaseModel
 class RefreshToken(BaseModel):
     __tablename__ = "refresh_token"
 
-    token = Column(String(50))
-    user_id = Column(ForeignKey("user.id", ondelete="CASCADE"))
+    created_at = Column(DateTime)
+    user_id = Column(ForeignKey("user.id", ondelete="CASCADE"), unique=True)
 
     user = relationship("User", back_populates="refresh_token")
