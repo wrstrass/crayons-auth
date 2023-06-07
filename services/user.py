49ebd731c1a8635ba69447d6109a1f5009d0c1f6
Anton <wrstrass@gmail.com>
2023-06-07 03:59:33 +0300

    Add get user by id

--- services/user.py
+++ services/user.py
@@ -11,3 +11,7 @@ class UserService:
 
     async def search(self, search_schema: UserSearchSchema) -> list[User]:
         return await self._user_repository.filter((User.username.startswith(search_schema.username),))
+
+    async def get_by_id(self, user_id: int) -> str:
+        user = await self._user_repository.get_by_id(user_id)
+        return user.username
--- views/user.py
+++ views/user.py
@@ -8,3 +8,7 @@ router = APIRouter(prefix="/user")
 @router.post(f"/search")
 async def search(search_schema: UserSearchSchema, service: UserService = Depends(UserService)) -> list[UserSchema]:
     return await service.search(search_schema)
+
+@router.get("/{user_id}")
+async def get_by_id(user_id: int, service: UserService = Depends(UserService)) -> str:
+    return await service.get_by_id(user_id)
